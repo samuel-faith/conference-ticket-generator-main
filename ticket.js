ee728a8e46120a4e0c744a1c8795e0f212aa5741
samuel faith <adelajafaithsamuel@gmail.com>
2025-01-18 13:04:10 +0100

    CSS Complete Styling and JS Functions

--- ticket.js
+++ ticket.js
@@ -1,21 +1,28 @@
-$("form").submit(function(e) {
-    e.preventDefault();
+const form = $("form");
 
-    var fullName = $("#full-name").value;
-    var email = $("#email").value;
+const dropArea = $("#drop-area");
+const fileInput = $("#file-input");
+//const uploadedImage = $(".uploaded-image");
+const messageAction = $("#message-action");
+const fileAction = $(".file-action");
+const removeImage = $("#remove-image");
+const changeImage = $("#change-image");
+const uploadHint = $("#upload-hint");
 
-    localStorage.setItem("fullName", fullName);
-    localStorage.setItem("email", email);
+const textInput = $(".required");
 
-    window.location.href = "./success.html";
-})
-
-var fullName = localStorage.getItem("fullName");
-var email = localStorage.getItem("email");
-
-if (fullName && email) {
-    $(".header1 h1").textContent = 'Congrats, $("fullName")! Your ticket is ready.'
-    $(".header1 p").textContent = 'We have emailed your ticket to $("email") and will send updates in the run up to the event.'
+const formData = {
+    image: '',
+    name: '',
+    email: '',
+    githubUsername: ''
 }
 
-localStorage.clear();
+dropArea.addEventListener("click", function() {
+    fileInput.click();
+})
+
+dropArea.addEventListener("dragover", function(event) {
+    event.preventDefault();
+    return;
+})
